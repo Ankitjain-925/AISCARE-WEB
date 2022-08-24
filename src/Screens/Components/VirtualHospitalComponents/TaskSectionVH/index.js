b1d72393c530f7bdbdf9f144085e3b30edf4d793
ruchi1234 <info.webnexus@gmail.com>
2022-08-24 14:25:10 +0530

    css merge

--- src/Screens/Components/VirtualHospitalComponents/TaskSectionVH/index.js
+++ src/Screens/Components/VirtualHospitalComponents/TaskSectionVH/index.js
@@ -4294,7 +4294,7 @@ removeTask21 = (id) => {
         </Grid>
         <Grid className="taskDetailMob">
           {/* {tabvalue === 0 && <TabContainer> */}
-          <Grid className="taskCntntMng">
+          <Grid className="taskCntntMng vwTaskSelectTpCstm">
             <Grid container direction="row" alignItems="center">
               <Grid item xs={12} sm={6} md={7}>
                 <AppBar position="static" className="billTabs">
