b2a465ca2d6320a2ada00ea4a3125b01343450f9
Tanyawebnexus <tanya.webnexus@gmail.com>
2023-01-10 23:35:24 -0800

    taking pull

--- src/sitedata.js
+++ src/sitedata.js
@@ -1,9 +1,5 @@
 var data = {
-<<<<<<< HEAD
-  "path"          : "http://localhost:5001/api/v5",
-=======
   // "path": "http://localhost:5001/api/v5",
->>>>>>> 5876cebb4201a354e1b754b224b1a6a249652115
   // live_site"  : "https://aimedis.io/",
   "path"       : "https://virtualhospital.aidoc.io/api/v5",
   // "path"       : "https://sys.aimedis.io/api/v3",
